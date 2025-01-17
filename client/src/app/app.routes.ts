0522af596d27a174a90062ab23790c0074df4402
DarknessoPirate <darknesso539@gmail.com>
2025-01-17 15:35:37 +0100

    added more components(empty for now), ngx-toastr, route guards and routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', component: HomeComponent },
+   // make the paths children of a main root path to make the guards run on all of them, instead of individually specify guards for each path
+   {
+      path: '',
+      runGuardsAndResolvers: 'always',
+      canActivate: [authGuard],
+      children: [
+         { path: 'members', component: MemberListComponent},
+         { path: 'members/:id', component: MemberDetailsComponent },
+         { path: 'lists', component: ListsComponent },
+         { path: 'messages', component: MessagesComponent },
+      ]
+   },
+
+   { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
